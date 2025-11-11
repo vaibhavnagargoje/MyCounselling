6a17d015654a98df69f0f31ce2e3971c4a77deab
vaibhavnagargoje <vaibhavnagargoje381@gmail.com>
2025-11-11 19:55:16 +0530

    landing page updated or othr pages created

--- landing_page/urls.py
+++ landing_page/urls.py
@@ -7,4 +7,11 @@ app_name = 'landing_page'
 urlpatterns = [
     # Define your URL patterns here
     path('', views.index, name='index'),
+    path('colleges/', views.colleges, name='colleges'),
+    path('college-details/<int:college_id>/', views.college_details, name='college_details'),
+    path('college-predictor/',views.college_predictor, name='college_predictor'),
+    path('rank-predictor/',views.rank_predictor, name='rank_predictor'),
+    path('about-us/',views.about_us, name='about_us'),
+    path('careers/',views.careers, name='careers'),
+
 ]
--- landing_page/views.py
+++ landing_page/views.py
@@ -6,3 +6,21 @@ def index(request):
     if request.user.is_authenticated:
         return render(request, 'home/home.html')
     return render(request, 'landing_page/index.html')
+
+
+
+def colleges(request):
+    return render(request, 'landing_page/colleges.html')
+
+def college_details(request):
+    return render(request, 'landing_page/college-details.html')
+
+def college_predictor(request):
+    return render(request, 'landing_page/college-predictor.html')
+def rank_predictor(request):
+    return render(request, 'landing_page/rank-predictor.html')
+def about_us(request):
+    return render(request, 'landing_page/about-us.html')
+
+def careers(request):
+    return render(request,'landing_page/careers.html')
